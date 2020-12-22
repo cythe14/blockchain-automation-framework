e1fa902336b3e14bcfd3e4eebc55394d215aac44
git_username <git_email>
2020-12-22 12:19:22 +0800

    [ci skip] Deleting gitops release files
    Signed-off-by: git_username <git_email>

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ echo "Adding env variables..."
 export PATH=/root/bin:$PATH
 
 #Path to k8s config file
-KUBECONFIG=/home/blockchain-automation-framework/build/config
+KUBECONFIG=/home/abel/Documents/BAF/blockchain-automation-framework/build/config
 
 
 echo "Running the playbook..."
-exec ansible-playbook -vv /home/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
+exec ansible-playbook -vvv /home/abel/Documents/BAF/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/abel/Documents/BAF/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/abel/Documents/BAF/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
