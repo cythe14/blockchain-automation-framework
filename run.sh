7cacb9320c9ef093e516c47b50eefbc8608578e7
cythe14 <cythe14@gmail.com>
2021-01-05 22:11:56 +0800

    [ci skip] Deleting gitops release files
    Signed-off-by: cythe14 <cythe14@gmail.com>

--- run.sh
+++ run.sh
@@ -13,4 +13,4 @@ KUBECONFIG=/home/abel/Documents/BAF/blockchain-automation-framework/build/config
 echo "Running the playbook..."
 exec ansible-playbook -vvv /home/abel/Documents/BAF/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/abel/Documents/BAF/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/abel/Documents/BAF/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
 
-exec ansible-playbook -vvv /home/abel/Documents/BAF/blockchain-automation-framework/platforms/r3-corda/configuration/deploy-cordapps.yaml -e "@path-to-network.yaml" -e "source_dir='/home/abel/Documents/cordapps/Advanced'"
+exec ansible-playbook -vvv /home/abel/Documents/BAF/blockchain-automation-framework/platforms/r3-corda/configuration/deploy-cordapps.yaml -e "@/home/abel/Documents/BAF/blockchain-automation-framework/build/network.yaml" -e "source_dir='/home/abel/Documents/cordapps/Advanced'"
