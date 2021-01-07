de4062093e2057f2eaf9901246eb52cb363b9133
cythe14 <cythe14@gmail.com>
2021-01-07 12:07:51 +0800

    [ci skip] Deleting gitops release files
    Signed-off-by: cythe14 <cythe14@gmail.com>

--- reset.sh
+++ reset.sh
@@ -11,4 +11,4 @@ KUBECONFIG=/home/abel/Documents/BAF/blockchain-automation-framework/build/config
 
 
 echo "Running the playbook..."
-exec ansible-playbook -vv /home/abel/Documents/BAF/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/abel/Documents/BAF/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/abel/Documents/BAF/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -e "reset='true'"
+exec ansible-playbook -vv /home/abel/Documents/BAF/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/abel/Documents/BAF/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/abel/Documents/BAF/blockchain-automation-framework/build/network_gke.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -e "reset='true'"
